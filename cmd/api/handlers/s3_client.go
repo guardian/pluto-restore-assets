d6515c6b6a77a36ce8e228cc3bca562ad628eca2
Richard Pryce <richard.pryce@guardian.co.uk>
2024-11-04 12:32:54 +0000

    Mock S3 client

---
+++ cmd/api/handlers/s3_client.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type S3ClientInterface interface {
+	ListObjectsV2(ctx context.Context, params *s3.ListObjectsV2Input, optFns ...func(*s3.Options)) (*s3.ListObjectsV2Output, error)
+	PutObject(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error)
+	HeadObject(ctx context.Context, params *s3.HeadObjectInput, optFns ...func(*s3.Options)) (*s3.HeadObjectOutput, error)
+}
